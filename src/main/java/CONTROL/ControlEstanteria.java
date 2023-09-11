f8ab5c9a273a14db1631de2e08d9c851b2e2520e
= <85245534+Leo-Spj@users.noreply.github.com>
2023-09-11 17:35:57 -0500

    + funcion busqueda binaria, sin implementacion

--- src/main/java/CONTROL/ControlEstanteria.java
+++ src/main/java/CONTROL/ControlEstanteria.java
@@ -3,21 +3,20 @@ package CONTROL;
 
 import MODELO.Libro;
 
-public class ControlEstanteria<L extends Libro>  {
-    
-    
+public class ControlEstanteria<L extends Libro> {
 
-    private Libro listaLibros[] = new Libro[1] ;
+
+    private Libro listaLibros[] = new Libro[1];
     private int size;
-    
+
     public ControlEstanteria() {
-        this.listaLibros =  new Libro[1];
+        this.listaLibros = new Libro[1];
         this.size = 0;
     }
 
-    public L obtenerUnLibro(long isbn){
-        for(Libro libro : listaLibros){
-            if(libro.getISBN() == isbn){
+    public L obtenerUnLibro(long isbn) {
+        for (Libro libro : listaLibros) {
+            if (libro.getISBN() == isbn) {
                 return (L) libro;
             }
         }
@@ -28,15 +27,15 @@ public class ControlEstanteria<L extends Libro>  {
         return listaLibros;
     }
 
-    public void listarLibros(){ // Este listado es para mostrar en consola, no usar, solo pruebas
-        for(Libro libro : listaLibros){
+    public void listarLibros() { // Este listado es para mostrar en consola, no usar, solo pruebas
+        for (Libro libro : listaLibros) {
             System.out.println(libro.getTitulo());
         }
     }
 
-    public void agregarLibro(L libro){
-        if(listaLibros[listaLibros.length-1] == null){
-            listaLibros[listaLibros.length-1] = libro;
+    public void agregarLibro(L libro) {
+        if (listaLibros[listaLibros.length - 1] == null) {
+            listaLibros[listaLibros.length - 1] = libro;
         } else {
             Libro listaLibrosAux[] = new Libro[listaLibros.length + 1];
             for (int i = 0; i < listaLibros.length; i++) {
@@ -47,27 +46,27 @@ public class ControlEstanteria<L extends Libro>  {
         }
     }
 
-    public void removerLibro(L libro){
-        for(int i = 0; i < listaLibros.length; i++){
-            if(listaLibros[i] == libro){
+    public void removerLibro(L libro) {
+        for (int i = 0; i < listaLibros.length; i++) {
+            if (listaLibros[i] == libro) {
                 listaLibros[i] = null;
 
                 Libro AuxIzquierda[] = new Libro[i];
                 Libro AuxDerecha[] = new Libro[listaLibros.length - i - 1];
 
-                for(int j = 0; j < AuxIzquierda.length; j++){
+                for (int j = 0; j < AuxIzquierda.length; j++) {
                     AuxIzquierda[j] = listaLibros[j];
                 }
-                for(int j = 0; j < AuxDerecha.length; j++){
+                for (int j = 0; j < AuxDerecha.length; j++) {
                     AuxDerecha[j] = listaLibros[j + i + 1];
                 }
 
                 listaLibros = new Libro[listaLibros.length - 1];
 
-                for(int j = 0; j < AuxIzquierda.length; j++){
+                for (int j = 0; j < AuxIzquierda.length; j++) {
                     listaLibros[j] = AuxIzquierda[j];
                 }
-                for(int j = 0; j < AuxDerecha.length; j++){
+                for (int j = 0; j < AuxDerecha.length; j++) {
                     listaLibros[j + AuxIzquierda.length] = AuxDerecha[j];
                 }
 
@@ -81,52 +80,52 @@ public class ControlEstanteria<L extends Libro>  {
 
     public void deBurbuja(String atributo) {
         //se ordenana por anioPublicacion:
-        for (int i = 0; i < listaLibros.length; i++){
-            for (int j = 0; j < listaLibros.length-1; j++){
-                if (atributo.equals("anioPublicacion")){
-                    if (listaLibros[j].getAnioPublicacion() > listaLibros[j+1].getAnioPublicacion()){
+        for (int i = 0; i < listaLibros.length; i++) {
+            for (int j = 0; j < listaLibros.length - 1; j++) {
+                if (atributo.equals("anioPublicacion")) {
+                    if (listaLibros[j].getAnioPublicacion() > listaLibros[j + 1].getAnioPublicacion()) {
                         Libro aux = listaLibros[j];
-                        listaLibros[j] = listaLibros[j+1];
-                        listaLibros[j+1] = aux;
+                        listaLibros[j] = listaLibros[j + 1];
+                        listaLibros[j + 1] = aux;
                     }
-                } else if (atributo.equals("ISBN")){
-                    if (listaLibros[j].getISBN() > listaLibros[j+1].getISBN()){
+                } else if (atributo.equals("ISBN")) {
+                    if (listaLibros[j].getISBN() > listaLibros[j + 1].getISBN()) {
                         Libro aux = listaLibros[j];
-                        listaLibros[j] = listaLibros[j+1];
-                        listaLibros[j+1] = aux;
+                        listaLibros[j] = listaLibros[j + 1];
+                        listaLibros[j + 1] = aux;
                     }
                 }
-                if (atributo.equals("titulo")){
-                    if (listaLibros[j].getTitulo().compareTo(listaLibros[j+1].getTitulo()) > 0){
+                if (atributo.equals("titulo")) {
+                    if (listaLibros[j].getTitulo().compareTo(listaLibros[j + 1].getTitulo()) > 0) {
                         Libro aux = listaLibros[j];
-                        listaLibros[j] = listaLibros[j+1];
-                        listaLibros[j+1] = aux;
+                        listaLibros[j] = listaLibros[j + 1];
+                        listaLibros[j + 1] = aux;
                     }
-                } else if (atributo.equals("autor")){
-                    if (listaLibros[j].getAutor().compareTo(listaLibros[j+1].getAutor()) > 0){
+                } else if (atributo.equals("autor")) {
+                    if (listaLibros[j].getAutor().compareTo(listaLibros[j + 1].getAutor()) > 0) {
                         Libro aux = listaLibros[j];
-                        listaLibros[j] = listaLibros[j+1];
-                        listaLibros[j+1] = aux;
+                        listaLibros[j] = listaLibros[j + 1];
+                        listaLibros[j + 1] = aux;
                     }
                 }
             }
         }
     }
 
-    public void porSeleccion_String(String atributo){
+    public void porSeleccion_String(String atributo) {
         int aux;
         int indice;
-        for (int i = 0; i < listaLibros.length; i++){
+        for (int i = 0; i < listaLibros.length; i++) {
             indice = i;
             String pequeño = "";
-            if (atributo.equals("titulo")){
+            if (atributo.equals("titulo")) {
                 pequeño = listaLibros[i].getTitulo();
             } else if (atributo.equals("autor")) {
                 pequeño = listaLibros[i].getAutor();
             }
-            for (int j = i; j < listaLibros.length; j++){
-                if (atributo.equals("titulo")){
-                    if (listaLibros[j].getTitulo().compareTo(pequeño) < 0){
+            for (int j = i; j < listaLibros.length; j++) {
+                if (atributo.equals("titulo")) {
+                    if (listaLibros[j].getTitulo().compareTo(pequeño) < 0) {
                         pequeño = listaLibros[j].getTitulo();
                         indice = j;
                     }
@@ -246,51 +245,49 @@ public class ControlEstanteria<L extends Libro>  {
             }
         }*/
 
-    
-
 
     // ------------------ Métodos de búsqueda : ------------------
 
-    public int busquedaOrdenada(int array[], int valor){
+    public int busquedaOrdenada(int array[], int valor) {
 
         //Busqueda secuencial
-        int indMayor = array.length-1;
+        int indMayor = array.length - 1;
         int ind = 0;
-        while(ind<indMayor && valor != array[ind]) {
+        while (ind < indMayor && valor != array[ind]) {
             ind++;
         }
-        if(valor == array[ind]){
+        if (valor == array[ind]) {
             return ind;
-        }else{
+        } else {
             return -1;
         }
     }
 
-    public Libro[] busquedaSecuencial(String atributo, String buscar){
+    public Libro[] busquedaSecuencial(String atributo, String buscar) {
         Libro[] resultados = new Libro[0];
-        for (int i = 0; i < listaLibros.length; i++){
-            if (atributo.equals("Titulo")){
-                if (listaLibros[i].getTitulo().toLowerCase().indexOf(buscar.toLowerCase()) != -1){
+        for (int i = 0; i < listaLibros.length; i++) {
+            if (atributo.equals("Titulo")) {
+                if (listaLibros[i].getTitulo().toLowerCase().indexOf(buscar.toLowerCase()) != -1) {
                     Libro[] aux = new Libro[resultados.length + 1];
-                    for (int j = 0; j < resultados.length; j++){
+                    for (int j = 0; j < resultados.length; j++) {
                         aux[j] = resultados[j];
                     }
                     aux[resultados.length] = listaLibros[i];
                     resultados = aux;
                 }
-            } else if (atributo.equals("ISBN")){
-                if (String.valueOf(listaLibros[i].getISBN()).indexOf(buscar) != -1){
+            } else if (atributo.equals("ISBN")) {
+                if (String.valueOf(listaLibros[i].getISBN()).indexOf(buscar) != -1) {
                     Libro[] aux = new Libro[resultados.length + 1];
-                    for (int j = 0; j < resultados.length; j++){
+                    for (int j = 0; j < resultados.length; j++) {
                         aux[j] = resultados[j];
                     }
                     aux[resultados.length] = listaLibros[i];
                     resultados = aux;
                 }
-            } else if (atributo.equals("Autor")){
-                if (listaLibros[i].getAutor().toLowerCase().startsWith(buscar.toLowerCase())){
+            } else if (atributo.equals("Autor")) {
+                if (listaLibros[i].getAutor().toLowerCase().startsWith(buscar.toLowerCase())) {
                     Libro[] aux = new Libro[resultados.length + 1];
-                    for (int j = 0; j < resultados.length; j++){
+                    for (int j = 0; j < resultados.length; j++) {
                         aux[j] = resultados[j];
                     }
                     aux[resultados.length] = listaLibros[i];
@@ -300,4 +297,36 @@ public class ControlEstanteria<L extends Libro>  {
         }
         return resultados;
     }
+
+    // busqueda binaria, primero se ordena mediante la funcion deBurbuja que recibe por el atributo a ordenar
+
+    public int busquedaBinaria(String atributo, String valor){// por ahora solo funciona para ISBN
+
+        long clave = Integer.parseInt(valor);
+
+        if (atributo.equals("ISBN")){
+            deBurbuja("ISBN");
+        }
+
+        //busqueda binaria segun libro
+        int central, bajo, alto;
+        long valorCentral;
+        bajo = 0;
+        alto = listaLibros.length -1;
+        while (bajo <= alto){
+            central = (bajo+alto)/2;
+            valorCentral = listaLibros[central].getISBN();
+            if (clave == valorCentral){
+                return central;
+            } else if (clave < valorCentral) {
+                alto = central - 1;
+            } else {
+                bajo = central + 1;
+            }
+        }
+        return -1;
+    }
+
+
+
 }
